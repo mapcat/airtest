2fe3d27d15615e428af2807689a78499ef1f174c
mapcat <mapcat@users.noreply.github.com>
2025-07-06 04:08:45 -0400

    Update extract_codes.py

--- air/scripts/extract_codes.py
+++ air/scripts/extract_codes.py
@@ -2,7 +2,7 @@ import csv
 
 iata_codes = set()
 
-with open('airports.csv', newline='', encoding='utf-8') as infile:
+with open('../data/airports.csv', newline='', encoding='utf-8') as infile:
     reader = csv.reader(infile, delimiter=';')
     next(reader)  # Skip header
     for row in reader:
@@ -11,7 +11,7 @@ with open('airports.csv', newline='', encoding='utf-8') as infile:
 
 sorted_codes = sorted(iata_codes)
 
-with open('airports-master.csv', 'w', newline='', encoding='utf-8') as outfile:
+with open('../data/airports-master.csv', 'w', newline='', encoding='utf-8') as outfile:
     writer = csv.writer(outfile)
     for code in sorted_codes:
         writer.writerow([code])
